6845c29ff75a3573fec902499af8d6cb5ea905c2
RavySaksena <ravysaksena5@gmail.com>
2025-11-29 16:52:19 +0700

    home screen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:uas/src/presentation/pages/home/home_screen.dart' as internal;
+
+.
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) => const internal.HomeScreen();
+}
